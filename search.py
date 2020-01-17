438b4ab08cc03faa1a52f9fa13411b3656c82977
Bastiaan <bastiaan@fea.st>
2020-01-17 15:08:23 +0100

    Implement dot product

--- search.py
+++ search.py
@@ -12,7 +12,8 @@ def opendoc(doc):
 def generatesqrmatrix(dataframe):
     """Generates a squared weighted term matrix from a `dataframe`. Returns the updated weighted term matrix."""
     dataframe = dataframe[0] # get the first entry from the tuple
-    for tuple in dataframe.itertuples(): # create a tuple from every line in the dataframe
+    dataframesqr = dataframe.copy()
+    for tuple in dataframesqr.itertuples(): # create a tuple from every line in the dataframe
         tuplen = len(tuple) # get the length of the tuple
         for i in range(2, tuplen): # for every digit in the tuple (starting from 2), do the loop
             tupvalue = tuple[i]
@@ -20,8 +21,8 @@ def generatesqrmatrix(dataframe):
                 current_row = tuple[0] # get the current row which is the first item in the tuple
                 header = tuple._fields[i] # the name of the field is the label of the namedtuple
                 tupvalue = tupvalue * tupvalue # square the tupvalue
-                dataframe.at[(current_row), header] = tupvalue
-    return dataframe
+                dataframesqr.at[(current_row), header] = tupvalue
+    return dataframesqr
 
 def generatedveclen(twmatrix):
     """
@@ -36,8 +37,23 @@ def generatedveclen(twmatrix):
         totals.append(total_sqrt)
     return totals
 
+def calcdotprod(query, dataframe, document):
+    """
+    Calculates the dot product for the `document` provided. The `query` is provided as a list and uses the dataframe to calculate the dot product for this document.
+    """
+    dataframe = dataframe[0] # get the first entry from the tuple
+    currentframe = dataframe[['Unnamed: 0', 'appeltaart']].copy() # copy the existing dataframe to a new dataframe called currentframe
+    totalscore = 0
+    for tuple in currentframe.itertuples(): # create a tuple from every line in the dataframe
+        if tuple[1] in query: # if the term is in the query, add the frequency to the totalscore
+            totalscore += tuple[2]
+    del currentframe # delete the copied dataframe to save on memory
+    return totalscore
+
+
 dataframe = opendoc("test_data/recepten.csv")
-print(dataframe[1])
+#print(dataframe)
 twmatrix = generatesqrmatrix(dataframe)
-print(twmatrix)
-print(generatedveclen(twmatrix))
+#print(twmatrix)
+#print(generatedveclen(twmatrix))
+print(calcdotprod(['appel', 'deeg'], dataframe, 'appeltaart'))
