c2db5aa847bf61f6a569b92ecff2c5a4dee6e8a1
Bastiaan <bastiaan@fea.st>
2020-01-23 00:01:48 +0100

    Create results page

--- site.py
+++ site.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from search import *
 app = Flask(__name__)
 
 from flask import Flask
@@ -6,13 +7,15 @@ from flask import render_template
 from flask import request
 app = Flask(__name__)
 
+aaaa = [[1, 0.3253, "doc1", "De oudste dochter.."], [2, 0.1623, "doc2", "ja dat was een ding"], [3, 0.0815, "doc3", "het verschil was vooral de Deutz.."], [5, 0.0227, "doc5", "en tot slot"]]
+
 @app.route('/')
 def home():
     return render_template('landing.html')
 
 @app.route('/results', methods=['POST'])
 def results():
-    return "This is a placeholder"
+    return render_template('results.html', query = request.form["query"], results = aaaa)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD']=True
