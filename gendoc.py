bfd046a520f4cf3ab68c7d0bfc4067c45d42a3a2
Bastiaan <bastiaan@fea.st>
2020-01-22 16:04:19 +0100

    Implement tf matrix

--- gendoc.py
+++ gendoc.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def cleantext(text):
     """This function cleans the supplied text from illegal characters, such as interpunction,
     upper case letters and numbers. It also generates the document term frequencies."""
@@ -19,7 +20,7 @@ def cleantext(text):
             word_frequencies[i] = 1
     return word_frequencies
 
-def gentermfreqmtx(doclist, folderpath):
+def gentermfreq(doclist, folderpath):
     """
     Function generates a term frequency matrix for the documents provided in the list 
     `doclist`. The `folderpath` to the folder containing the documents needs to be provided.
@@ -33,7 +34,32 @@ def gentermfreqmtx(doclist, folderpath):
         wordcounts[document] = cleanedtxt
     return wordcounts
 
+def generatematrix(wordcounts):
+    wordlist = list()
+    header = list()
+    matrix = list()
+    for document in wordcounts:
+        header.append(document)
+        for term in wordcounts[document]:
+            if term not in wordlist:
+                wordlist.append(term)
+    matrix.append(header)
+    for word in wordlist:
+        row = list()
+        row.append(word)
+        for document in header:
+            if word in wordcounts[document]:
+                frequency = wordcounts[document].get(word) # get the frequency from the key from the assoc. dictionary
+                row.append(frequency)
+            else:
+                row.append(0)
+        matrix.append(row)
+    header.insert(0, "")
+    return matrix
+
 # EXAMPLE
-doclist = ["test1.txt", "test2.txt", "test3.txt"]
+doclist = ["test1.txt", "test2.txt", "test3.txt", "test4.txt"]
 folderpath = "test_data"
-print(gentermfreqmtx(doclist, folderpath))
+dict = gentermfreq(doclist, folderpath)
+#print(dict)
+print(generatematrix(dict))
