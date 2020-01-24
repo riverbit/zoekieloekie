ae751708079fd4c8296c03d0d12e2e0c7a75e851
Restyled.io <commits@restyled.io>
2020-01-24 13:47:07 +0000

    Restyled by autopep8

--- site.py
+++ site.py
@@ -1,19 +1,21 @@
+from flask import request
+from flask import render_template
 from flask import Flask
 from search import *
 app = Flask(__name__)
 
-from flask import Flask
-from flask import render_template
-from flask import request
 app = Flask(__name__)
+
 
 @app.route('/test')
 def test():
     return render_template('index.html')
 
+
 @app.route('/')
 def home():
     return render_template('landing.html')
+
 
 @app.route('/results', methods=['POST'])
 def results():
@@ -25,10 +27,11 @@ def results():
     dotproducts = getdotprod(query, dataframe)
     similarities = sim(dotproducts, query, vectorlength)
     results = rank(similarities)
-    return render_template('return.html', query = rawquery, results = results)
+    return render_template('return.html', query=rawquery, results=results)
+
 
 if __name__ == "__main__":
-    app.config['TEMPLATES_AUTO_RELOAD']=True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['DEBUG'] = True
     app.config['SERVER_NAME'] = "127.0.0.1:5000"
     app.run()
